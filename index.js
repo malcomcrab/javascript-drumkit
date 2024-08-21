db1a980d5ace592ec69f9bdca383680a13f51c11
malcomcrab <malcomcrabesq@outlook.com>
2024-08-21 18:24:37 +0100

    page b/ground image, drum parts + sound trigger function added

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 
 const kitContainer = document.getElementById('kit-container')
 
+const body = document.querySelector('body')
+body.style.backgroundImage = "url('images/background.jpg')"
+
+const drumKitParts = ['Kick', 'Snare', 'HiHat', 'OpenHat',
+                     'Tom', 'Clap', 'Boom', 'Ride', 'Tink']
+
+
+for (i = 0; i <= 8; i++){
+    const kitPiece = document.createElement('div')
+    kitPiece.id = `kitPiece${i}`
+    kitPiece.class = 'kitPiece'
+    kitPiece.innerText = drumKitParts[i] 
+    kitContainer.appendChild(kitPiece)
+
+    kitPiece.addEventListener('click', event =>{
+        drumTrigger(kitPiece.innerText)
+        
+    })
+}
+
+
+function drumTrigger(drumPart){
+    
+    let drumLower = drumPart.toLowerCase()
+    console.log(drumLower)
+    let audio = new Audio(`sounds/${drumLower}.wav`);
+    audio.play();
+}
